2160ec49a44586223bb245bc7bef11f1627e5e70
Muzammil hussain <trigonstech@gmail.com>
2021-06-15 02:19:27 +0600

    first commit

---
+++ app/views/vendor/js.php
@@ -0,0 +1,7 @@
+<script src="<?php echo $this->config->item('js_url'); ?>popper.min.js"></script>
+<script src="<?php echo $this->config->item('js_url'); ?>bootstrap.min.js"></script>
+<script src="<?php echo $this->config->item('js_url'); ?>online_hiring_advocate.js"></script>
+<script src="<?php echo $this->config->item('js_url'); ?>sidebar.js"></script>
+<script src="<?php echo $this->config->item('js_url'); ?>datepicker.js"></script>
+<script src="<?php echo $this->config->item('js_url'); ?>admin_panel.js"></script>
+
